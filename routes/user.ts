dad4f66a9c1de81782a2ec4f1978d88e6b93c1b6
kingpaul <paulpulgares24@gmail.com>
2023-03-30 14:45:01 +0800

    authorization for editing profile

--- routes/user.ts
+++ routes/user.ts
@@ -8,6 +8,7 @@ import {
   validateLogin,
   validateId,
   requireAuth,
+  isAuthor,
 } from "../middlewares/";
 import catchAsync from "../utils/catchAsync";
 import * as user from "../controllers/user";
@@ -18,9 +19,14 @@ router.post("/login", validateLogin, catchAsync(user.login));
 
 router.use(catchAsync(requireAuth));
 
-router.put("/update", catchAsync(user.edit));
-
 router.post("/uploadImage", upload.any(), catchAsync(user.uploadAvatar));
+
+router.put(
+  "/:id/update",
+  validateId,
+  catchAsync(isAuthor),
+  catchAsync(user.edit)
+);
 
 router.get("/:id", validateId, catchAsync(user.getUser));
 router.get("/:id/posts", validateId, catchAsync(user.getUserPost));
