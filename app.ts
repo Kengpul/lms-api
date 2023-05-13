c31e7373d604190b2b40c55f078a0cac65a088b5
kengpul <paulpulgares24@gmail.com>
2023-05-13 13:58:04 +0800

    test

--- app.ts
+++ app.ts
@@ -61,3 +61,4 @@ app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server running on Port ${PORT}`));
+
